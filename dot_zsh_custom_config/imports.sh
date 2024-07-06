9785da9d0f67b77ab352b4cc0880fc5493d38ec6
Jorge Morán <jams@jorgemoran.me>
2024-07-05 22:15:47 -0500

    Forgot to add pipx for ARM installs

--- dot_zsh_custom_config/imports.sh
+++ dot_zsh_custom_config/imports.sh
@@ -2,6 +2,7 @@
 [ -d "/usr/local/go/bin" ] && export PATH=$PATH:/usr/local/go/bin
 [ -d "${HOME}/.cargo/bin" ] && export PATH=$PATH:${HOME}/.cargo/bin
 [ -d "${HOME}/.local/bin" ] && export PATH="$PATH:$HOME/.local/bin"
+[ -d "${HOME}/.nix-profile/bin" ] && export PATH="$HOME/.nix-profile/bin:$PATH"
 
 if [ -x "$(which atuin)" ]
 then
